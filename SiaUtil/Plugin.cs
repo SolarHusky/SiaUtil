e5872812cd940e047f7800187aee0b825c35592c
Auros <41306347+aurosnex@users.noreply.github.com>
2019-09-30 22:45:52 -0400

    bruh

---
+++ SiaUtil/Plugin.cs
@@ -0,0 +1,69 @@
+﻿using IPA;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+using SiaUtil.Visualizers;
+
+namespace SiaUtil
+{
+    public class Plugin : IBeatSaberPlugin
+    {
+        public void Init(IPALogger logger)
+        {
+            Logger.log = logger;
+        }
+
+        public void OnApplicationStart()
+        {
+            _ = RadialIcon();
+        }
+
+        public void OnApplicationQuit()
+        {
+            Logger.log.Debug("OnApplicationQuit");
+        }
+
+        public void OnFixedUpdate()
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+            
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            if (scene.name == "MenuCore")
+            {
+                var a = new GameObject();
+                var b = a.AddComponent<WorldSpaceRadial>();
+                b.Create();
+                a.transform.position = new Vector3(0f, 5f, 5f);
+            }
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+
+        }
+
+        private static Sprite _radial;
+        public static Sprite RadialIcon()
+        {
+            if (_radial == null)
+            {
+                _radial = Utilities.LoadSpriteFromResources("SiaUtil.Assets.Circle.png");
+                return _radial;
+            }
+            else
+                return _radial;
+        }
+    }
+}
---
+++ SiaUtil/Visualizers/WorldSpaceRadial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SiaUtil.Visualizers
+{
+    public class WorldSpaceRadial : MonoBehaviour
+    {
+        private Image _radialImage;
+        private GameObject _radial;
+
+        public void Create()
+        {
+            Canvas canvas = gameObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.WorldSpace;
+            CanvasScaler canvasScaler = gameObject.AddComponent<CanvasScaler>();
+            canvasScaler.scaleFactor = 10.0f;
+            canvasScaler.dynamicPixelsPerUnit = 10f;
+            gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1f);
+            gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1f);
+
+            _radial = new GameObject();
+            _radialImage = _radial.AddComponent<Image>();
+            _radial.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 2f);
+            _radial.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 2f);
+            _radial.transform.localScale = new Vector3(4, 4, 4);
+            _radial.transform.localPosition = Vector3.zero;
+
+            _radialImage.sprite = Plugin.RadialIcon();
+            _radialImage.type = Image.Type.Filled;
+            _radialImage.fillMethod = Image.FillMethod.Radial360;
+            _radialImage.fillOrigin = (int)Image.Origin360.Top;
+            _radialImage.fillClockwise = true;
+            _radialImage.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+
+        }
+
+    }
+}
